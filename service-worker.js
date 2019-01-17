3bcfe69fc03ae241feb15f68902099824bc1a55d
Fajar Septian <fajarajay10@gmail.com>
2019-01-17 14:52:46 +0700

    penerapan workbox

--- service-worker.js
+++ service-worker.js
@@ -1,81 +1,180 @@
-const CACHE_NAME = "pwasepakbola";
-var urlsToCache = [
-    "/",
-    "/nav.html",
-    "/index.html",
-    "/detailtim.html",
-    "/detailplayer.html",
-    "/detailmatch.html",
-    "/pages/about.html",
-    "/pages/contact.html",
-    "/pages/favorit.html",
-    "/pages/home.html",
-    "/pages/matches.html",
-    "/css/materialize.min.css",
-    "/js/materialize.min.js",
-    "/js/nav.js",
-    "/js/api.js",
-    "/js/indexeddb.js",
-    "/js/database.js",
-    "/js/idb.js",
-    "/js/matches.js",
-    "/js/matchFav.js",
-    "/js/playerFav.js",
-    "/js/standings.js",
-    "/js/teamFav.js",
-    "/js/teams.js",
-    "/js/helper.js",
-    "/image/notif.png",
-    "/icon.png"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-///Menyimpan Aset ke Cache
-self.addEventListener("install", function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
+if (workbox) {
+    console.log(`Workbox berhasil dimuat`);
 
-///Menyimpan Cache Secara Dinamis
-self.addEventListener("fetch", function (event) {
-    var base_url = "https://api.football-data.org/v2/";
-    if (event.request.url.indexOf(base_url) > -1) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(function (cache) {
-                return fetch(event.request).then(function (response) {
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                })
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request, {
-                ignoreSearch: true
-            }).then(function (response) {
-                return response || fetch(event.request);
-            })
-        )
-    }
-});
+} else {
+    console.log(`Workbox gagal dimuat`);
+}
 
-///Menghapus Cache Lama
-self.addEventListener("activate", function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.map(function (cacheName) {
-                    if (cacheName != CACHE_NAME) {
-                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+///Precaching App Shell
+workbox.precaching.precacheAndRoute([{
+        url: '/',
+        revision: '1'
+    },
+    {
+        url: '/nav.html',
+        revision: '1'
+    },
+    {
+        url: '/index.html',
+        revision: '1'
+    },
+    {
+        url: '/detailtim.html',
+        revision: '1'
+    },
+    {
+        url: '/detailplayer.html',
+        revision: '1'
+    },
+    {
+        url: '/detailmatch.html',
+        revision: '1'
+    },
+    {
+        url: '/pages/about.html',
+        revision: '1'
+    },
+    {
+        url: '/pages/contact.html',
+        revision: '1'
+    },
+    {
+        url: '/pages/favorit.html',
+        revision: '1'
+    },
+    {
+        url: '/pages/home.html',
+        revision: '1'
+    },
+    {
+        url: '/pages/matches.html',
+        revision: '1'
+    },
+    {
+        url: '/css/materialize.min.css',
+        revision: '1'
+    },
+    {
+        url: '/js/materialize.min.js',
+        revision: '1'
+    },
+    {
+        url: '/js/nav.js',
+        revision: '1'
+    },
+    {
+        url: '/js/api.js',
+        revision: '1'
+    },
+    {
+        url: '/js/indexeddb.js',
+        revision: '1'
+    },
+    {
+        url: '/js/database.js',
+        revision: '1'
+    },
+    {
+        url: '/js/idb.js',
+        revision: '1'
+    },
+    {
+        url: '/js/matches.js',
+        revision: '1'
+    },
+    {
+        url: '/js/matchFav.js',
+        revision: '1'
+    },
+    {
+        url: '/js/playerFav.js',
+        revision: '1'
+    },
+    {
+        url: '/js/standings.js',
+        revision: '1'
+    },
+    {
+        url: '/js/teamFav.js',
+        revision: '1'
+    },
+    {
+        url: '/js/teams.js',
+        revision: '1'
+    },
+    {
+        url: '/js/helper.js',
+        revision: '1'
+    },
+    {
+        url: '/image/notif.png',
+        revision: '1'
+    },
+    {
+        url: '/icon.png',
+        revision: '1'
+    },
+]);
+
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'images',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 hari
+            }),
+        ],
+    }),
+);
+
+workbox.routing.registerRoute(
+    new RegExp('https://api.football-data.org/v2/'),
+    workbox.strategies.staleWhileRevalidate()
+)
+
+// Menyimpan cache dari CSS Google Fonts
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+);
+
+// Menyimpan cache untuk file font selama 1 tahun
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'google-fonts-webfonts',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            }),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    workbox.strategies.cacheFirst()
+);
+
+workbox.routing.registerRoute(
+    new RegExp('/css/materialize.min.css'),
+    workbox.strategies.cacheFirst()
+);
+
+workbox.routing.registerRoute(
+    new RegExp('.*\.png'),
+    workbox.strategies.cacheFirst()
+);
 
 //siapkan dulu service worker untuk menerima datanya
 self.addEventListener('push', function (event) {
